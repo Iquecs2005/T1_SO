864ef3744f27d1b38a38a8a1e644518c91ff0987
Henrique Carvalho <iquecarvalho2005@gmail.com>
2025-10-16 16:51:07 -0300

    Kernel updates

--- KernelSim.c
+++ KernelSim.c
@@ -13,47 +13,86 @@
 #define FIFOINT "Interuptions"
 #define NUM_AP 5
 
+int sharedMemIds[NUM_AP];
+ProcessData sharedMemPtr[NUM_AP];
+int currentRunningProcess = 0;
+
 void interruptionHandler();
 void syscallHandler();
 
 //TODO: criar as filas para D1 e D2
 int main(void)
 {
-    int sharedMemIds[5];
-    int* sharedMemPtr[5];
+    signal(SIGUSR1, interruptionHandler);
+    signal(SIGUSR2, syscallHandler);
 
     for (int i = 0; i < NUM_AP; i++) 
     {
-        if((pid_t pid = fork() < 0))
-        {
-            perror("Error creating child");
-            exit(1);
-        }
-        if(pid == 0)
-        {
-            break;
-        }
-        sharedMemIds[i] = shmget(IPC_PRIVATE, sizeof(ProcessData), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
-        sharedMemPtr[i] = shmat(sharedMemIds[i], 0, 0);
-        ProcessData* pc =  sharedMemPtr[i];
-        pc->pid = sharedMemIds[i];
-        pc->kernelPid = getpid();
-        pc->programCounter = 0;
+        sharedMemIds[i] = -1; 
     }
 
     if(pid == 0)
     {
         //descobrir como checar se foi criado ou nao
         //trat
-        signal(SIGUSR2, syscallHandler);
         execlp("./AplicationProcess", sharedMemIds[i]);
     }
     else
     {
         //kernell vai rodar comecando do primeiro entao ele vai checar se foi criado e permitir que vai rodar
         //capturar interrupcoes e chamadas do sistema, chamada do sistema faz
-        signal(SIGUSR1, interruptionHandler);
+    }
     return 0;
+}
+
+int RoundRobinScheduling()
+{
+    ProcessData* currentProcessData;
+
+    //Stop current process from running
+    currentProcessData = sharedMemPtr[currentRunningProcess];
+    kill(currentProcessData->pid, SIGSTOP);
+    currentProcessData->status = READY;
+    
+    currentRunningProcess = (currentRunningProcess + 1) % NUM_AP;
+
+    currentProcessData = sharedMemPtr[currentRunningProcess];
+    kill(currentProcessData->pid, SIGCONT);
+    currentProcessData->status = READY;
+}
+
+int AlocateProcessMemory(int i)
+{
+    if (sharedMemIds[i] == -1)
+    {
+        return -1;
+    }
+
+    sharedMemIds[i] = shmget(IPC_PRIVATE, sizeof(ProcessData), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    sharedMemPtr[i] = shmat(sharedMemIds[i], 0, 0);
+    ProcessData* pc =  sharedMemPtr[i];
+    pc->pid = -1;
+    pc->memoryId = sharedMemIds[i];
+    pc->kernelPid = getpid();
+    pc->status = NEW;
+    pc->programCounter = 0;
+
+    return sharedMemIds[i];
+}
+
+int CreateProcess(int i)
+{
+    AlocateProcessMemory(i);
+
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        execlp("./AplicationProcess", sharedMemIds[i]);
+    }
+    sharedMemPtr[i]->pid = pid;
+    pc->status = RUNNING;
+
+    return sharedMemIds[i];
 }
 
 void interruptionHandler()
--- ProcessData.h
+++ ProcessData.h
@@ -1,7 +1,15 @@
+#define NEW 0
+#define READY 1
+#define RUNNING 2
+#define BLOCKED 3
+#define FINISHED 4
+
 typedef struct processData ProcessData;
 struct processData
 {
     int pid;
+    int memoryId;
     int kernelPid;
+    int status;
     int programCounter;
 };
